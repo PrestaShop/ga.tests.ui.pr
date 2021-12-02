9b50c7e0c2921be5d4ffd32b40d3dcffd030f213
Boubker Bribri <boubker.bribri@prestashop.com>
2021-12-02 18:43:42 +0100

    first commit

---
+++ scripts/apache/setup.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -e
+# Get vars
+WORKSPACE=$1
+PS_DIR=$2
+PHP_VERSION=$3
+
+# Install apache
+add-apt-repository ppa:ondrej/php -y
+apt update && apt install -y apache2 libapache2-mod-php$PHP_VERSION
+
+# Enable rewrite mode
+a2enmod rewrite actions alias
+
+# Copy apache vhost and set Documentroot
+cp -f $WORKSPACE/scripts/apache/apache-vhost /etc/apache2/sites-available/000-default.conf
+sed -e "s?%BUILD_DIR%?$(echo $PS_DIR)?g" --in-place /etc/apache2/sites-available/000-default.conf
+
+# Restart apache after giving permission
+chmod 777 -R $PS_DIR
+service apache2 restart
