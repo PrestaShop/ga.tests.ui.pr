4e0362f2852db6d6e472df213aaae9e4f43e09bd
Boubker Bribri <boubker.bribri@prestashop.com>
2021-12-03 15:19:50 +0100

    DEBUG 8

--- scripts/apache/setup.sh
+++ scripts/apache/setup.sh
@@ -3,10 +3,9 @@
 set +x
 
 # Get vars
-WORKSPACE=$(cd "$( dirname "$0" )/../../" && pwd)
-echo $WORKSPACE
-PS_DIR=$1
-PHP_VERSION=$2
+WORKSPACE=$1
+PS_DIR=$2
+PHP_VERSION=$3
 
 # Install apache
 add-apt-repository ppa:ondrej/php -y
@@ -16,10 +15,11 @@ apt update && apt install -y apache2 libapache2-mod-php$PHP_VERSION
 a2enmod rewrite actions alias
 
 # Copy apache vhost and set Documentroot
-cp -f $WORKSPACE/scripts/apache/apache-vhost /etc/apache2/sites-available/000-default.conf
+cp -f ./scripts/apache/apache-vhost /etc/apache2/sites-available/000-default.conf
 sed -e "s?%BUILD_DIR%?$(echo $WORKSPACE/$PS_DIR)?g" --in-place /etc/apache2/sites-available/000-default.conf
 cat /etc/apache2/sites-available/000-default.conf
 
 # Restart apache after giving permission
-chmod 777 -R $PS_DIR/
+ls $WORKSPACE/$PS_DIR/
+chmod 777 -R $WORKSPACE/$PS_DIR/
 service apache2 restart
