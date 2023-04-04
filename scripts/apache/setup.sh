3236af0f5560e54539ef09b963e4715eaa4a980a
Progi1984 <progi1984@gmail.com>
2023-04-04 12:54:37 +0000

    Support for SSL

--- scripts/apache/setup.sh
+++ scripts/apache/setup.sh
@@ -11,11 +11,12 @@ PHP_VERSION=$3
 apt update && apt install -y libapache2-mod-php$PHP_VERSION
 
 # Enable rewrite mode
-a2enmod rewrite actions alias
+a2enmod rewrite actions alias ssl
 
 # Copy apache vhost and set Documentroot
 cp -f ./scripts/apache/apache-vhost /etc/apache2/sites-available/000-default.conf
 sed -e "s?%BUILD_DIR%?$(echo $WORKSPACE/$PS_DIR)?g" --in-place /etc/apache2/sites-available/000-default.conf
+sed -e "s?%MKCERT_DIR%?$(echo $WORKSPACE)?g" --in-place /etc/apache2/sites-available/000-default.conf
 
 # Restart apache after giving permission
 chmod 777 -R $WORKSPACE/$PS_DIR/
